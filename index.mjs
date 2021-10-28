962c15ed34aeffaa73709835262bacf14bd9f79c
ObscuritySRL <stev.p@outlook.com>
2021-10-28 19:31:22 -0400

    Include and export more structures

--- index.mjs
+++ index.mjs
@@ -10,6 +10,7 @@ export { default as FastifyManager } from './managers/FastifyManager';
 
 // Structures
 export { default as BaseCommandInteraction } from './structures/BaseCommandInteraction';
+export { default as BaseMessageComponent } from './structures/BaseMessageComponent';
 export { default as ButtonInteraction } from './structures/ButtonInteraction';
 export { default as CommandInteraction } from './structures/CommandInteraction';
 export { default as CommandInteractionOptionResolver } from './structures/CommandInteractionOptionResolver';
@@ -17,9 +18,11 @@ export { default as ContextMenuInteraction } from './structures/ContextMenuInter
 export { default as Interaction } from './structures/Interaction';
 export { default as Member } from './structures/Member';
 export { default as Message } from './structures/Message';
+export { default as MessageActionRow } from './structures/MessageActionRow';
 export { default as MessageAttachment } from './structures/MessageAttachment';
 export { default as MessageButton } from './structures/MessageButton';
-export { default as MessageSelectMenu } from './structures/MessageSelectMenu';
 export { default as MessageComponentInteraction } from './structures/MessageComponentInteraction';
+export { default as MessagePayload } from './structures/MessagePayload';
+export { default as MessageSelectMenu } from './structures/MessageSelectMenu';
 export { default as User } from './structures/User';
 export { default as Webhook } from './structures/Webhook';
